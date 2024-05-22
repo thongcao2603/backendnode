eb0b5915b0e0557097e759c30e7d582aa3dafc21
thongcao2603 <133794541+thongcao2603@users.noreply.github.com>
2024-05-23 00:04:35 +0700

    check api key

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 'use strict'
 
 const express = require('express')
+const { apiKey } = require('../auth/checkAuth')
 const router = express.Router()
 
+router.use(apiKey)
 router.use('/api/v1', require('./access'))
 
 module.exports = router
